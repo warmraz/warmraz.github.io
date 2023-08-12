0656e0cdfc712277e9676977bc58dd3b3f926847
warmraz <86163246+warmraz@users.noreply.github.com>
2023-08-12 18:12:08 +0200

    Add files via upload

---
+++ keyboards.py
@@ -0,0 +1,11 @@
+from aiogram.types import WebAppInfo
+from aiogram import types
+
+web_app = WebAppInfo(url='https://warmraz.github.io/')
+
+keyboard = types.ReplyKeyboardMarkup(
+    keyboard=[
+        [types.KeyboardButton(text='Site', web_app=web_app)]
+    ],
+    resize_keyboard=True
+)
---
+++ main.py
@@ -0,0 +1,23 @@
+from aiogram import Bot, Dispatcher
+
+import asyncio
+
+token = "6699984953:AAH-LnjhzHjP-UblOkWx2v7DmEntowC6GAw"
+bot = Bot(token=token)
+dp = Dispatcher(bot=bot)
+
+
+async def main():
+    from handlers import dp
+
+    try:
+        await dp.start_polling()
+    finally:
+        await bot.session.close()
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        print("Bot stopped!")
